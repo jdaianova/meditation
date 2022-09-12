280c1a0903b072b237541be3f65b4bc5f390d099
Julia <jdaianova@gmail.com>
2022-09-12 11:57:15 +0200

    2 commit

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
-const timer = 0.5;
-var countdown = timer * 60;
+const startButton = document.querySelector('#button');
+const timer = document.querySelector('.timer');
 
-function countDown() {
-    const timer = document.querySelector('.timer');
-    let min = Math.floor(countdown / 60);
-    let sec = countdown % 60;
-    if (sec <= 0) {
-        sec = "0" + sec;
-    };
-    if (min <= 0) {
-        min = "0" + min;
-    };
-    timer.textContent = `${min} : ${sec}`;
-    countdown--;
-    if (countdown < 0) {
-        countdown = 0;
-        clearInterval(timerId);
-    };
+startButton.onclick = () => {
+    const timeOfMeditation = document.querySelector('#input').valueAsNumber;
+    document.querySelector('#input').style.display = 'none';
+    document.querySelector('.enter-time').style.display = 'none';
+    var countdown = timeOfMeditation * 60;
+    if (timeOfMeditation > 0) {
+    document.querySelector('.audio').play(); 
+    document.querySelector('.video').play();
+
+    let timerId = setInterval(() => {
+        let min = Math.floor(countdown / 60);
+        let sec = countdown % 60;
+        if (sec <= 9) {
+            sec = "0" + sec;
+        };
+        if (min <= 0) {
+            min = "0" + min;
+        };
+        timer.textContent = `${min} : ${sec}`;
+        countdown--;
+        if (countdown < 0) {
+            countdown = 0;
+            clearInterval(timerId);
+            history.back();
+        };
+    }, 1000);
+    startButton.textContent = 'Enjoy the sounds ';
 }
-
-let timerId = setInterval(countDown, 1000);
+};
